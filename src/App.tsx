88cc83c5c9ba5a7bde388c85d80aed2799ae0bdf
Yoga_Sama <eichelberger.y@gmail.com>
2025-02-03 01:27:24 +0100

    HTML Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import './App.css';
+
 function App() {
-  return '¯\\_(ツ)_/¯';
+  return (
+    <>
+      <header className="header">Pokedex</header>
+      <main className="main">
+        <ul className="grid">
+          <li>Pokemon 1</li>
+          <li>Pokemon 2</li>
+          <li>Pokemon 3</li>
+          <li>Pokemon 4</li>
+          <li>Pokemon 5</li>
+          <li>Pokemon 6</li>
+          <li>Pokemon 7</li>
+          <li>Pokemon 8</li>
+          <li>Pokemon 9</li>
+        </ul>
+        <button className="show-more">Afficher plus</button>
+      </main>
+    </>
+  );
 }
 
 export default App;
