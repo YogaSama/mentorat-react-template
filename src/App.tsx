942a18ab6f24383595c83885b736332ae5ac6c64
Yoga_Sama <eichelberger.y@gmail.com>
2025-02-04 02:23:51 +0100

    Add controlled form.

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,16 @@ const DEFAULT_LIMIT = 9;
 function App() {
   const [limit, setLimit] = useState<number>(DEFAULT_LIMIT);
   const [pokemons, error, loading] = usePokemons(limit, 0);
+  const [filter, setFilter] = useState<string>('');
 
   const handleShowMoreClick = () => {
     setLimit((limit) => limit + DEFAULT_LIMIT);
+  };
+
+  const handleFilterInputChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setFilter(event.target.value.toLowerCase());
   };
 
   return (
@@ -18,15 +25,31 @@ function App() {
       <header className="header">Pokedex</header>
       <main className="main">
         {error != null && 'Une erreur est survenue !'}
-        {pokemons != null && (
-          <ul className="grid">
-            {pokemons.map((pokemon) => (
-              <li key={pokemon.name}>
-                <PokemonCard name={pokemon.name} />
-              </li>
-            ))}
-          </ul>
-        )}
+        <form className="filters">
+          <label htmlFor="filter-input">Filter</label>
+          <input
+            id="filter-input"
+            type="text"
+            placeholder="pikachu"
+            value={filter}
+            onChange={handleFilterInputChange}
+          />
+        </form>
+        <ul className="grid">
+          {pokemons?.map((pokemon) => (
+            <li
+              key={pokemon.name}
+              style={{
+                display:
+                  filter === '' || pokemon.name.includes(filter)
+                    ? 'block'
+                    : 'none',
+              }}
+            >
+              <PokemonCard name={pokemon.name} />
+            </li>
+          ))}
+        </ul>
         <button
           className="show-more"
           onClick={handleShowMoreClick}
