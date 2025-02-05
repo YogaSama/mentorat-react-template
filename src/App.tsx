75ff6f1144fd64460e0f3adca9ed95d448a60566
Yoga_Sama <eichelberger.y@gmail.com>
2025-02-05 01:31:43 +0100

    1. Structure HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
 function App() {
-  return '¯\\_(ツ)_/¯';
+  return (
+    <>
+      <header className="header">Pokedex</header>
+      <main className="main">
+        <div className="tools">
+          <button className="show-more">Voir plus</button>
+          <div className="shiny">
+            <label htmlFor="shiny-checkbox">shiny</label>
+            <input id="shiny-checkbox" type="checkbox" />
+          </div>
+        </div>
+        <div className="list">
+          <div className="item">
+            #1 pokemon
+            <img
+              className="icon"
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+            />
+          </div>
+          <div className="item">
+            #2 pokemon
+            <img
+              className="icon"
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+            />
+          </div>
+          <div className="item">
+            #3 pokemon
+            <img
+              className="icon"
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
