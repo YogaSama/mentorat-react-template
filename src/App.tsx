08f4e2661b0f23331e93aa3b448cb275c1206cad
Yoga_Sama <eichelberger.y@gmail.com>
2025-02-05 01:56:43 +0100

    3. Création dynamique d'éléments (liste).

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
+import { useState } from 'react';
 import PokemonItem from './PokemonItem';
 
+function createPokemon(id: number) {
+  return {
+    id: id,
+    name: 'pokemon',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+  };
+}
+
+function createPokemons(offset: number, count: number) {
+  return [...new Array(count)].map((v, i) => createPokemon(offset + i));
+}
+
 function App() {
+  const [pokemons, setPokemons] = useState(createPokemons(1, 3));
   return (
     <>
       <header className="header">Pokedex</header>
       <main className="main">
         <div className="tools">
-          <button className="show">Voir plus</button>
+          <button
+            className="show"
+            onClick={() => {
+              setPokemons((previous) => [
+                ...previous,
+                ...createPokemons(previous.length + 1, 3),
+              ]);
+            }}
+          >
+            Voir plus
+          </button>
           <div className="shiny">
             <label htmlFor="shiny-checkbox">shiny</label>
             <input id="shiny-checkbox" type="checkbox" />
           </div>
         </div>
         <div className="list">
-          <PokemonItem
-            id={1}
-            name="pokemon"
-            url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-          />
-          <PokemonItem
-            id={2}
-            name="pokemon"
-            url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-          />
-          <PokemonItem
-            id={3}
-            name="pokemon"
-            url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-          />
+          {pokemons.map((pokemon) => (
+            <PokemonItem
+              key={pokemon.id}
+              id={pokemon.id}
+              name={pokemon.name}
+              url={pokemon.url}
+            />
+          ))}
         </div>
       </main>
     </>
